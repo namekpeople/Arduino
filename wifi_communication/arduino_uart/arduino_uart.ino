341a12fdd99a2496d76813bed2249c317f4b4ca3
namekpeople <116502520+namekpeople@users.noreply.github.com>
2022-11-20 11:12:09 +0700

    2 rfid trong 2 function
    2 function rfid cho mỗi thẻ ra/vào

--- wifi_communication/arduino_uart/arduino_uart.ino
+++ wifi_communication/arduino_uart/arduino_uart.ino
@@ -4,7 +4,7 @@ const byte RX = 2;
 const byte TX = 3;
 SoftwareSerial mySerial = SoftwareSerial(RX, TX);
 
-//lcd, button, servo
+//lcd, cảm biến
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -17,12 +17,15 @@ int count = 0;
 #include <Servo.h>
 
 #define RST_PIN         9
-//#define SS_RA         8           // Configurable, see typical pin layout above
+#define SS_RA         8           // Configurable, see typical pin layout above
 #define SS_VAO         10          // Configurable, see typical pin layout above
+
+//servo
 Servo sv_vao, sv_ra;
 String sv;
 bool vao, ra;
 MFRC522 mfrc522_vao(SS_VAO, RST_PIN);   // Create mfrc522_ra instance.
+MFRC522 mfrc522_ra(SS_RA, RST_PIN);   // Create mfrc522_ra instance.
 String UIDSend = "";
 
 void setup()
@@ -42,75 +45,139 @@ void setup()
   mfrc522_vao.PCD_Init(); // Init mfrc522_vao card
   sv_vao.attach(5);
   sv_vao.write(0);
+  //rfid_ra
+  mfrc522_ra.PCD_Init(); // Init mfrc522_vao card
+  sv_ra.attach(6);
+  sv_ra.write(0);
 }
 
 void loop()
 {
+  hien_thi();
   read_ESP();
-  //  hien_thi();
-  send_ESP();
+  the_vao();
+  the_ra();
 }
 
 void read_ESP() {
   while (mySerial.available())
   {
     sv = mySerial.read();
-//    if (sv == '\n') 
-//      break;
-//    else if (sv == 1 || sv == 2){
-//      Serial.println(sv);
-//      break;
-//    }
     vao = sv.indexOf('1');
     ra = sv.indexOf('2');
   }
-//  if (sv == '1' || sv == '2')
-  if (vao>0 || ra>0)
+  if (vao>0)
   {
     sv_vao.write(100);
     delay(3000);
     sv_vao.write(0);
     vao = false;
+  }
+  else if (ra>0)
+  {
+    sv_ra.write(100);
+    delay(3000);
+    sv_ra.write(0);
     ra = false;
-//    sv = "";
   }
 }
 
-void send_ESP() {
+void the_vao() {
   // Look for new cards
   if ( ! mfrc522_vao.PICC_IsNewCardPresent())
     return;
   // Select one of the cards
   if ( ! mfrc522_vao.PICC_ReadCardSerial())
     return;
-  String content = "";
+  String content = "ci";
   for (int i = 0; i < mfrc522_vao.uid.size; i++)
   {
     content.concat(String(mfrc522_vao.uid.uidByte[i], HEX));
   }
   content.toUpperCase();
-//  Serial.println(content);
   sendUID(content);
   mfrc522_vao.PICC_HaltA();
   mfrc522_vao.PCD_StopCrypto1();
 }
 
+void the_ra() {
+  // Look for new cards
+  if ( ! mfrc522_ra.PICC_IsNewCardPresent())
+    return;
+  // Select one of the cards
+  if ( ! mfrc522_ra.PICC_ReadCardSerial())
+    return;
+  String content = "co";
+  for (int i = 0; i < mfrc522_ra.uid.size; i++)
+  {
+    content.concat(String(mfrc522_ra.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  sendUID(content);
+  mfrc522_ra.PICC_HaltA();
+  mfrc522_ra.PCD_StopCrypto1();
+}
+
 void sendUID(String content) {
   UIDSend = "";
-  String pre = "";
-  //Đóng gói UID
-//  if(digitalRead(A0)==0)
-//    {
-//            pre = "ci";
-//    }
-//  if(digitalRead(A1)==0)
-//    {
-//            pre = "co";
-//    }
-  UIDSend = pre + content;
+  UIDSend = content;
   Serial.print("Send to ESP: ");
   Serial.println(UIDSend);
   Serial.flush();
   mySerial.println(UIDSend);
   mySerial.flush();
+}
+
+void hien_thi(){
+  if(digitalRead(A3)==0)
+  {
+          a = 1;
+  }
+  else
+  {
+    a = 0;
+   }
+
+   if(digitalRead(A2)==0)
+  {
+          b = 1;
+  }
+  else
+  {
+    b = 0;
+   }
+   if(digitalRead(A1)==0)
+  {
+          c = 1;
+  }
+  else
+  {
+    c= 0;
+   }
+
+   if(digitalRead(A0)==0)
+  {
+          d = 1;
+  }
+  else
+  {
+    d = 0;
+  }
+
+   count = a+b+c+d;
+
+  if (count == 4) 
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Full slot, Sorry");
+  }
+  else
+  {
+    lcd.setCursor(0,1);
+    lcd.print(4 - count);
+    lcd.print(" Slot available");
+  }
+
+ lcd.setCursor(3,0);
+ lcd.print("UTC Parking");
 }
